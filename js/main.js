23ac8fcb303aa018d0540cb2138fcfd62064998e
Md Shayon <mdshayon0@gmail.com>
2020-09-17 15:22:35 -1200

    three menu has been made with one maga menu and one sidebar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
-$('.ui.dropdown')
-  .dropdown()
-;
+// MEGA MENU START
+const mega_menu_icon = document.getElementById("mega-menu-icon");
+const mega_menu_box = document.getElementById('mega-menu-box');
 
-$('.special.cards .image').dimmer({
-  on: 'hover'
+
+
+
+
+
+const mouseOverEvent = () => { 
+    mega_menu_box.classList.add('box-preview');
+    console.log( mega_menu_box.classList);
+    mega_menu_box.addEventListener("mouseover", mouseOverEvent);
+    mega_menu_box.addEventListener("mouseout", mouseOutEvent);
+};
+
+
+const mouseOutEvent = () => { 
+    mega_menu_box.classList.remove('box-preview');
+    console.log( mega_menu_box.classList);
+    console.log( mega_menu_box.classList);
+
+};
+
+
+
+mega_menu_icon.addEventListener("mouseover", mouseOverEvent);
+mega_menu_icon.addEventListener("mouseout",mouseOutEvent);
+// MEGA MENU ENDS
+
+
+
+
+
+
+// SIDE MENU START
+const side_menu_icon = document.getElementById('sidemenu');
+const menu_box = document.getElementById('menu-box');
+const close_icon = document.getElementById('close-icon');
+
+console.log(menu_box);
+side_menu_icon.addEventListener('click', ()=>{
+    menu_box.style.display = "block";
 });
+close_icon.addEventListener('click', ()=>{
+    menu_box.style.display = "none";
+});
+// SIDE MENU ENDS 
+
+
